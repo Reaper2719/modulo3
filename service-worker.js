3ef06bd9d01a486d860b762c362479902d29ce03
Reaper2719 <jdiazr2704@gmail.com>
2025-06-17 15:58:25 -0500

    modulo 3 0.0.7

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,39 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('modulo3-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './main.js',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+
+const CACHE_NAME = 'modulo3-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match('./index.html'))
+    )
   );
 });
