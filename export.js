4b975a2b216cbf84d6ed2b664fa6f56ad5c005ef
Reaper2719 <jdiazr2704@gmail.com>
2025-06-18 13:35:34 -0500

    modulo 4 0.1.8

--- export.js
+++ export.js
@@ -1,11 +1,31 @@
-function exportarDatos() {
-  const data = localStorage.getItem("respuestas_modulo2");
-  if (!data) return alert("No hay datos guardados.");
-  const blob = new Blob([data], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "respuestas_modulo2.json";
-  a.click();
-  URL.revokeObjectURL(url);
+
+async function exportarDatos() {
+  const request = indexedDB.open("registrosDB", 1);
+
+  request.onsuccess = function (event) {
+    const db = event.target.result;
+    const tx = db.transaction("registros", "readonly");
+    const store = tx.objectStore("registros");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+      const datos = JSON.stringify(getAll.result, null, 2);
+      const blob = new Blob([datos], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "modulo4_registros.json";
+      a.click();
+      URL.revokeObjectURL(url);
+    };
+
+    getAll.onerror = function () {
+      alert("❌ Error al leer registros");
+    };
+  };
+
+  request.onerror = function () {
+    alert("❌ No se pudo abrir la base de datos");
+  };
 }
